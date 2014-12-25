067cef94737eae68808a3c7559d9c577d03fb947
Rohan <rohan@katipally.com>
2014-12-25 18:31:36 +0000

    Allow users to enter data on a form, and hook it up to the database.

--- Gemfile
+++ Gemfile
@@ -45,3 +45,5 @@ end
 # gem 'debugger', group: [:development, :test]
 
 gem "twitter-bootstrap-rails"
+
+gem 'simple_form'
--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,22 @@
 class PicsController < ApplicationController
+	
 	def index
 		@pics = Pic.all
 	end
+
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		@pic = Pic.create(pic_params)
+		redirect_to pics_path
+	end
+
+	private
+
+	def pic_params
+		params.require(:pic).permit(:lesson, :emotion, :learned)
+	end
+
 end
